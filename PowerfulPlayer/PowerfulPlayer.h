22ee79320204888ab25ea961dfbb08c999e56e79
= <=>
2020-01-08 18:43:31 +0300

    Initial project setup

---
+++ PowerfulPlayer/PowerfulPlayer.h
@@ -0,0 +1,19 @@
+//
+//  PowerfulPlayer.h
+//  PowerfulPlayer
+//
+//  Created by Necmettin Ayata on 8.01.2020.
+//  Copyright © 2020 Orhan Ayata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PowerfulPlayer.
+FOUNDATION_EXPORT double PowerfulPlayerVersionNumber;
+
+//! Project version string for PowerfulPlayer.
+FOUNDATION_EXPORT const unsigned char PowerfulPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PowerfulPlayer/PublicHeader.h>
+
+
---
+++ PowerfulPlayerExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowerfulPlayerExamples
+//
+//  Created by Necmettin Ayata on 8.01.2020.
+//  Copyright © 2020 Orhan Ayata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
